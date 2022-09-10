296c4b83af5112c3e9c60b1c215a419a5f41bc56
EndocrineBAMSc <skeb90@googlemail.com>
2022-09-10 12:28:59 +0200

    Test if still runs
    Test Commit

---
+++ GameJamTeamBased/Assets/Scripts/OnMouseOverHighlight.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnMouseOverHighlight : MonoBehaviour
+{
+
+    private SpriteRenderer spriteR;
+    private Sprite originalSprite;
+    private bool isHighlighted;
+    public Sprite highlightedSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fetch the sprites
+        spriteR = GetComponent<SpriteRenderer>();
+        originalSprite = spriteR.sprite;
+
+        //ensure correct starting point for bool (prolly unnecessary)
+        isHighlighted = false;
+    }
+
+    void OnMouseOver()
+    {
+        isHighlighted = true;
+    }
+
+    void OnMouseExit()
+    {
+        isHighlighted = false;
+    }
+    
+    private void highlightSprite()
+    {
+        spriteR.sprite = highlightedSprite;
+    }
+
+    private void resetSprite()
+    {
+        spriteR.sprite = originalSprite;
+    }
+
+    /*war wohl irgendwie notwendig, es direkt in 
+     * den Funktionen oben zu machen hat die Sprite nicht neu gerendert 
+     * -> es wurde einfach schwarz */
+    void Update()
+    {
+        
+        //Leertaste markiert alle Ojekte
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isHighlighted = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isHighlighted = false;
+        }
+
+
+        //Wechselt Sprites entsprechend bool value
+        if (isHighlighted)
+        {
+            highlightSprite();
+        } else
+        {
+            resetSprite();
+        }
+    }
+}
---
+++ GameJamTeamBased/Assets/Scripts/OnSpacePressHighlight.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnSpacePressHighlight : OnMouseOverHighlight
+{
+    /*
+    private SpriteRenderer spriteR;
+    private Sprite originalSprite;
+    private bool isHighlighted;
+    public Sprite highlighted;
+    */
+
+    // Start is called before the first frame update
+    
+    /*
+    void Start()
+    {
+        //Fetch the sprites
+        spriteR = GetComponent<SpriteRenderer>();
+        originalSprite = spriteR.sprite;
+        
+        //ensure correct starting point for bool (prolly unnecessary)
+        isHighlighted = false;
+    }
+    */
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isHighlighted = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isHighlighted = false;
+        }
+
+        if (isHighlighted)
+        {
+            highlightSprite();
+        }
+        else
+        {
+            resetSprite();
+        }
+
+    }
+}
