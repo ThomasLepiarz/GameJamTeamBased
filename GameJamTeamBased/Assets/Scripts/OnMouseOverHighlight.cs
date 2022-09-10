7b7923607eb0589c0340b107f478f58efe599e26
EndocrineBAMSc <skeb90@googlemail.com>
2022-09-10 17:29:32 +0200

    ChangeCursorComplete
    Changing Cursor now works and old script is improved and shortened aswell.

--- GameJamTeamBased/Assets/Scripts/OnMouseOverHighlight.cs
+++ GameJamTeamBased/Assets/Scripts/OnMouseOverHighlight.cs
@@ -9,7 +9,6 @@ public class OnMouseOverHighlight : MonoBehaviour
 
     private SpriteRenderer spriteR;
     private Sprite originalSprite;
-    private bool isHighlighted;
     public Sprite highlightedSprite;
 
     // Start is called before the first frame update
@@ -18,22 +17,10 @@ public class OnMouseOverHighlight : MonoBehaviour
         //Fetch the sprites
         spriteR = GetComponent<SpriteRenderer>();
         originalSprite = spriteR.sprite;
-
-        //ensure correct starting point for bool (prolly unnecessary)
-        isHighlighted = false;
     }
 
     //ändert bool value nach Mauspositionsabfrage (erster Frame wo Maus Collider berührt)
-    void OnMouseOver()
-    {
-        isHighlighted = true;
-    }
 
-    void OnMouseExit()
-    {
-        isHighlighted = false;
-    }
-    
     private void highlightSprite()
     {
         spriteR.sprite = highlightedSprite;
@@ -44,31 +31,14 @@ public class OnMouseOverHighlight : MonoBehaviour
         spriteR.sprite = originalSprite;
     }
 
-    /*war wohl irgendwie notwendig, es direkt in 
-     * den Funktionen oben zu machen hat die Sprite nicht neu gerendert 
-     * -> es wurde einfach schwarz */
-    void Update()
+    void OnMouseOver()
     {
-        
-        //Leertaste markiert alle Ojekte
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            isHighlighted = true;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            isHighlighted = false;
-        }
-
-
-        //Wechselt Sprites entsprechend bool value
-        if (isHighlighted)
-        {
-            highlightSprite();
-        } else
-        {
-            resetSprite();
-        }
+        highlightSprite();
     }
+
+    void OnMouseExit()
+    {
+        resetSprite();
+    }
+
 }
