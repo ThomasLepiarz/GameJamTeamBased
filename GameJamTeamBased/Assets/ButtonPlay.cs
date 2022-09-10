41cb0121e3cfa3832981fce4634f3496231e9a34
EndocrineBAMSc <skeb90@googlemail.com>
2022-09-10 09:09:16 +0200

    Main Menu
    Hackiges Main Menu angelegt.

---
+++ GameJamTeamBased/Assets/ButtonPlay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonPlay : MonoBehaviour
+{
+    //Transfer to next scene in builder
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Quit Game... duh
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
