30ef3aab63401485ac63257c970c764b011ab29f
EndocrineBAMSc <skeb90@googlemail.com>
2022-09-11 19:17:16 +0200

    Sounddesign Schlafzimmer

---
+++ GameJamTeamBased/Assets/Scripts/OnceSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnceSound : MonoBehaviour
+{
+    private bool hasPlayed;
+    public AudioSource oncePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!hasPlayed)
+        {
+            oncePlayer.Play();
+            hasPlayed = true;
+        }
+    }
+}
